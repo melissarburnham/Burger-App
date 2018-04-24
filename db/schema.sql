9cea2dd66e0f46e44a393a03091fc2e1b6c0ab33
Your Name <melissarburnham@gmail.com>
2018-04-23 20:35:38 -0400

    fix sql info

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,13 @@
 ### Schema
 
-CREATE DATABASE cat_db;
-USE cat_db;
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+USE burger_db;
 
-CREATE TABLE cats
+CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
+	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+
+INSERT INTO burgers (name, devoured) VALUES ('Veggie', true);
+INSERT INTO burgers (name, devoured) VALUES ('Chicken', true);
+INSERT INTO burgers (name, devoured) VALUES ('BBQ', true);
